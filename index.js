1238dab9027a862be6630979d187e73e3c777430
emmanuel <emmanuel.lion@brl.fr>
2025-02-10 20:21:20 +0100

    save

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const { exec } = require('child_process');
-const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import { exec } from 'child_process';
+import { ChartJSNodeCanvas } from 'chartjs-node-canvas';
+import ora from 'ora';
+import chalk from 'chalk';
 
 const app = express();
 const port = 2002;
 
-// Fonction pour obtenir la taille d'un dossier
 function getFolderSize(folderPath) {
 	return new Promise((resolve, reject) => {
 		let totalSize = 0;
@@ -49,74 +52,93 @@ async function getLatestVersion(pkg) {
 }
 
 async function listPackages() {
-	const nodeModulesPath = path.join(__dirname, 'node_modules');
-	const packageJsonPath = path.join(__dirname, 'package.json');
+	const nodeModulesPath = path.join(process.cwd(), 'node_modules');
+	const packageJsonPath = path.join(process.cwd(), 'package.json');
+	if(!fs.existsSync(nodeModulesPath)){
+		throw "No node_modules found!";
+	}
+	if(!fs.existsSync(packageJsonPath)){
+		throw 'No package.json found!';
+	}
 
 	try {
 		const packageJson = JSON.parse(await fs.promises.readFile(packageJsonPath, 'utf-8'));
 		const dependencies = { ...packageJson.dependencies, ...packageJson.devDependencies };
 
 		let packageData = [];
-
 		for (const pkg of Object.keys(dependencies)) {
 			const pkgPath = path.join(nodeModulesPath, pkg);
 			if (fs.existsSync(pkgPath)) {
 				const size = await getFolderSize(pkgPath);
 				const installedVersion = dependencies[pkg];
 				const latestVersion = await getLatestVersion(pkg);
-				const outdated = latestVersion && installedVersion.replaceAll('^', '') !== latestVersion;
+				const outdated = latestVersion && installedVersion.replace('^', '') !== latestVersion;
 				packageData.push({ name: pkg, size, installedVersion, latestVersion, outdated });
 			}
 		}
-
 		packageData.sort((a, b) => b.size - a.size);
-
 		return packageData;
 	} catch (error) {
-		console.error("Erreur lors de la lecture des dépendances du package.json ou du dossier node_modules:", error);
-		return [];
+		throw "Erreur lors de la lecture des dépendances du package.json ou du dossier node_modules:";
 	}
 }
 
-const width = 800;
-const height = 400;
-const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height });
+// if (require.main === module) {
+	(async () => {
+		let spinner = ora({
+			text: "Reading packages",
+			spinner: "material",
+			color: "magenta",
+		}).start();
+		listPackages().then((result) => {
+			spinner.succeed();
+			console.log(chalk.green(JSON.stringify(result)));
+		}).catch((error) => {
+			spinner.fail();
+			console.log(chalk.red(error));
+		});
+	})();
+// }
 
-app.get('/chart', async (req, res) => {
-	const packageData = await listPackages();
-
-	const chartConfig = {
-		type: 'bar',
-		data: {
-			labels: packageData.map(pkg => pkg.name),
-			datasets: [{
-				label: 'Taille des packages (MB)',
-				data: packageData.map(pkg => pkg.size / 1024 / 1024),
-				backgroundColor: packageData.map(pkg => pkg.outdated ? 'rgba(255, 99, 132, 0.2)' : 'rgba(75, 192, 192, 0.2)'),
-				borderColor: packageData.map(pkg => pkg.outdated ? 'rgba(255, 99, 132, 1)' : 'rgba(75, 192, 192, 1)'),
-				borderWidth: 1
-			}]
-		},
-		options: {
-			responsive: false,
-			scales: {
-				y: {
-					beginAtZero: true
-				}
-			}
-		}
-	};
-
-	const image = await chartJSNodeCanvas.renderToBuffer(chartConfig);
-	res.set('Content-Type', 'image/png');
-	res.send(image);
-});
-
-app.get('/packages', async (req, res) => {
-	const packageData = await listPackages();
-	res.json(packageData);
-});
-
-app.listen(port, () => {
-	console.log(`Serveur en ligne sur http://localhost:${port}`);
-});
+// const width = 800;
+// const height = 400;
+// const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height });
+//
+// app.get('/chart', async (req, res) => {
+// 	const packageData = await listPackages();
+//
+// 	const chartConfig = {
+// 		type: 'bar',
+// 		data: {
+// 			labels: packageData.map(pkg => pkg.name),
+// 			datasets: [{
+// 				label: 'Taille des packages (MB)',
+// 				data: packageData.map(pkg => pkg.size / 1024 / 1024),
+// 				backgroundColor: packageData.map(pkg => pkg.outdated ? 'rgba(255, 99, 132, 0.2)' : 'rgba(75, 192, 192, 0.2)'),
+// 				borderColor: packageData.map(pkg => pkg.outdated ? 'rgba(255, 99, 132, 1)' : 'rgba(75, 192, 192, 1)'),
+// 				borderWidth: 1
+// 			}]
+// 		},
+// 		options: {
+// 			responsive: false,
+// 			scales: {
+// 				y: {
+// 					beginAtZero: true
+// 				}
+// 			}
+// 		}
+// 	};
+//
+// 	const image = await chartJSNodeCanvas.renderToBuffer(chartConfig);
+// 	res.set('Content-Type', 'image/png');
+// 	res.send(image);
+// });
+//
+// app.get('/packages', async (req, res) => {
+// 	const packageData = await listPackages();
+// 	res.json(packageData);
+// });
+//
+// app.listen(port, () => {
+// 	console.log(`Serveur en ligne sur http://localhost:${port}`);
+// });
---
+++ upgrade.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const filePackagePath = './package.json';
+const filePackageTest = "./frontend/package.json";
+
+fs.readFile(filePackagePath, 'utf8', (err, data) => {
+	const jsonData = JSON.parse(data);
+
+	let work = jsonData.version;
+	let NEW_VERSION =  work.split(".").slice(0, 2).join('.') + '.' + (parseInt(work.split(".")[2]) + 1);
+	jsonData.version = NEW_VERSION;
+
+	console.log(`NEW VERSION IS : ${jsonData.version} !!!`);
+
+	fs.writeFile(filePackagePath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+		console.log("Le fichier package a été mis à jour avec succès !");
+
+		exec('npm publish', (error, stdout, stderr) => {
+			if (error) {
+				console.error(`Erreur lors de l'exécution de la commande: ${error.message}`);
+				return;
+			}
+
+			if (stderr) {
+				console.error(`Erreur standard: ${stderr}`);
+				return;
+			}
+
+			fs.readFile(filePackageTest, 'utf8', (err, data) => {
+				const jsonTestData = JSON.parse(data);
+
+				jsonTestData.version = '^'+NEW_VERSION;
+
+				fs.writeFile(filePackageTest, JSON.stringify(jsonTestData, null, 2), 'utf8', (err) => {
+					console.log("Le fichier package de test a été mis à jour avec succès !");
+				});
+			});
+		});
+	});
+});
