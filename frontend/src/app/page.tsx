0bf126f8e3126d5194cc341e7973236fc61e3e6e
emmanuel <emmanuel.lion@brl.fr>
2025-02-12 15:27:03 +0100

    save before merge

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,35 @@
+'use client';
+
 import Card from "@/components/Card/Card";
+import {useEffect, useState} from "react";
+import Loader from "@/components/Loader/Loader";
 
 export default function Page() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        if(!data){
+            fetch('http://localhost:2002/data')
+                .then(res => res.json()
+                    .then(data => setData(data)))
+                .catch(err => console.log(err));
+        }
+    }, []);
+
   return (
     <div className="w-screen h-screen overflow-hidden bg-black/25 p-4">
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <Card />
-            <Card />
-            <Card />
-        </div>
-        <div className="min-h-[60vh] flex-1 rounded-xl bg-muted/50" />
-      </div>
+        {
+            data ? (
+                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                        <Card title={"Some data"} />
+                        <Card />
+                        <Card />
+                    </div>
+                    <div className="min-h-[60vh] flex-1 rounded-xl bg-muted/50" />
+                </div>
+            ) : <Loader />
+        }
     </div>
   );
 }
