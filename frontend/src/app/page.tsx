f0e3bccf4f92d527bbc210394c276293fb328ce0
emmanuel <emmanuel.lion@brl.fr>
2025-02-12 18:53:45 +0100

    save

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,14 +4,93 @@ import Card from "@/components/Card/Card";
 import {useEffect, useState} from "react";
 import Loader from "@/components/Loader/Loader";
 
+interface ProjectMetrics {
+    totalFiles: number;
+    totalLines: number;
+    averageComplexity: string;
+    totalCommentLines: number;
+    documentationCoverage: string;
+    totalAsyncCount: number;
+    duplicatePercentage: string;
+    dependenciesCount: number;
+    npmAuditResults: any;
+    lint: any;
+    startupTime: number;
+    ressources: any;
+    buildTime: number;
+    dependencies: any;
+}
+
+export interface NumberData {
+    text: string;
+    value: number;
+    suffix?: string;
+}
+
 export default function Page() {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState<null | ProjectMetrics>(null);
+    const [infos, setInfos] = useState<NumberData[]>([]);
 
     useEffect(() => {
         if(!data){
             fetch('http://localhost:2002/data')
                 .then(res => res.json()
-                    .then(data => setData(data)))
+                    .then(data => {
+                        setInfos([
+                            {
+                                text: "Number of dependencies : ",
+                                value: data.data.dependenciesCount
+                            },
+                            {
+                                text: "Build time : ",
+                                value: data.data.buildTime,
+                                suffix: "ms"
+                            },
+                            {
+                                text: "Average complexity : ",
+                                value: parseFloat(data.data.averageComplexity),
+                                suffix: "between 1 & 10 = good, 11 & 20 = ok, ..."
+                            },
+                            {
+                                text: "Startup time : ",
+                                value: data.data.startupTime,
+                                suffix: "ms"
+                            },
+                            {
+                                text: "Total number of lines : ",
+                                value: data.data.totalLines
+                            },
+                            {
+                                text: "Total number of totalFiles : ",
+                                value: data.data.totalLines
+                            },
+
+                        ]);
+                        console.log([
+                            {
+                                text: "Number of dependencies : ",
+                                value: data.data.dependenciesCount
+                            },
+                            {
+                                text: "Build time : ",
+                                value: data.data.buildTime
+                            },
+                            {
+                                text: "Average complexity : ",
+                                value: parseFloat(data.data.averageComplexity)
+                            },
+                            {
+                                text: "Startup time : ",
+                                value: data.data.startupTime
+                            },
+                            {
+                                text: "Total number of lines : ",
+                                value: data.data.totalLines
+                            },
+
+                        ])
+                        setData(data.data);
+                    }))
                 .catch(err => console.log(err));
         }
     }, []);
@@ -22,9 +101,9 @@ export default function Page() {
             data ? (
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
                     <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                        <Card title={"Some data"} />
-                        <Card />
-                        <Card />
+                        {infos.length > 0 && <Card title={"Some data"} description={"Some data"} data={infos} footer={"footer"} />}
+                        {/*<Card />*/}
+                        {/*<Card />*/}
                     </div>
                     <div className="min-h-[60vh] flex-1 rounded-xl bg-muted/50" />
                 </div>
--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,24 +1,42 @@
+'use client';
+
 import {CardUi, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import NumberFlow from "@number-flow/react";
+import {useEffect, useState} from "react";
+import {NumberData} from "@/app/page";
 
 interface CardProps {
-    title?: string;
-    description?: string;
-    content?: string;
-    footer?: string;
+    title: string;
+    description: string;
+    footer: string;
+    data: NumberData[];
 }
 
-export default function Card({title, description, content, footer}: CardProps) {
+export default function Card({title, description, footer, data}: CardProps) {
+    const [value, setValue] = useState<NumberData[]>(data);
+
     return (
         <CardUi className={"aspect-video rounded-xl bg-muted/50"}>
             <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{description}</CardDescription>
             </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
+            <CardContent className="flex flex-col">
+                {
+                    value.map((nb: NumberData, index: number) => {
+                        return <NumberFlow
+                            value={nb.value}
+                            prefix={nb.text}
+                            suffix={nb.suffix ? ` ${nb.suffix}` : ""}
+                            trend={1}
+                            key={index}
+                        />
+                    })
+                }
+
             </CardContent>
             <CardFooter>
-                <p>Card Footer</p>
+                <p>{footer}</p>
             </CardFooter>
         </CardUi>
     )
